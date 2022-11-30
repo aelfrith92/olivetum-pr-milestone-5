d5b00a54f2bd2e66909431f8da399cf169147c8a
Alfredo Morieri <fred.morieri@gmail.com>
2022-11-30 17:49:28 +0000

    Define templates

--- olivetum/settings.py
+++ olivetum/settings.py
@@ -17,6 +17,7 @@ development = os.environ.get("DEVELOPMENT")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
