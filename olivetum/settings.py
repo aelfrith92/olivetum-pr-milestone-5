5ce1426f983cc4454b0113ea9c3c6fb27e0af76a
Alfredo Morieri <fred.morieri@gmail.com>
2022-11-30 18:10:47 +0000

    Test email verification: success via manual edit

--- olivetum/settings.py
+++ olivetum/settings.py
@@ -90,6 +90,16 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
 WSGI_APPLICATION = 'olivetum.wsgi.application'
 
 
