e70b9591e6ab06fbbd728447f7148bb6b2a1bd73
Alfredo Morieri <fred.morieri@gmail.com>
2022-12-09 17:47:13 +0000

    add search form, js for hover to be fixed, style mobile behaviour

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,12 +6,13 @@ $( document ).ready(function() {
     function hoverButtons() {
         
         // Select the path tag inside the svg tag to modify its attribute 'stroke'
-        $('#shoppingBag').mouseover(function(event){
-            $('#shoppingBag path').attr('fill', '#fffff7');
-        });
-        $('#shoppingBag').mouseleave(function(event){
+        $('#shoppingBag .svgFill').mouseover(function(event){
             event.stopPropagation();
             $('#shoppingBag path').attr('fill', '#181D31');
+        });
+        $('#shoppingBag .svgFill').mouseleave(function(event){
+            event.stopPropagation();
+            $('#shoppingBag path').attr('fill', '#f8f3e5');
         });
     }
     hoverButtons();
