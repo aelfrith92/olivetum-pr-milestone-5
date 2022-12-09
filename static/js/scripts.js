71b95df579c6f7ad99ed14bd2c8c54c4fd9a0e77
Alfredo Morieri <fred.morieri@gmail.com>
2022-12-09 23:10:46 +0000

    Style auth login page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,13 +6,22 @@ $( document ).ready(function() {
     function hoverButtons() {
         
         // Select the path tag inside the svg tag to modify its attribute 'stroke'
-        $('#shoppingBag .svgFill').mouseover(function(event){
+        $('#shoppingBag').mouseover(function(event){
             event.stopPropagation();
             $('#shoppingBag path').attr('fill', '#181D31');
         });
-        $('#shoppingBag .svgFill').mouseleave(function(event){
+        $('#shoppingBag').mouseleave(function(event){
             event.stopPropagation();
             $('#shoppingBag path').attr('fill', '#f8f3e5');
+        });
+
+        $('#searchBtn').mouseover(function(event){
+            event.stopPropagation();
+            $('#searchBtn path').attr('fill', '#181D31');
+        });
+        $('#searchBtn').mouseleave(function(event){
+            event.stopPropagation();
+            $('#searchBtn path').attr('fill', '#f8f3e5');
         });
     }
     hoverButtons();
