836e3f153391b78d653ce6c7b013dc0c79787890
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-12 17:57:14 +0000

    Add the view about all products and related url lines at app and project level, arrange products and categories admin overviews

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_products(request):
+    """This view shows/retrieves all products"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
