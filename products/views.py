ccbe27c9f6ca596813ae82a8fdcbafdaeb4cbe06
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-14 16:21:23 +0000

    add product detail view and create related template

--- products/views.py
+++ products/views.py
@@ -12,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """A view to show the overview of a specific product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
