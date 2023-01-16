402e0af96eece5f2f6c36f3d7342909be85f6d85
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-16 22:02:42 +0000

    Add media queries breakpoints to style the filters on the product page, add filters and subfilters, prepare code for sorting, style the filter container, rename filter bar to main navbar and include main menu navigation in the base template

--- products/views.py
+++ products/views.py
@@ -6,12 +6,26 @@ def all_products(request):
     """This view shows/retrieves all products"""
 
     products = Product.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """A view to show the overview of a specific product"""
