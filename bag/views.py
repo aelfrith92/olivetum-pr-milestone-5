ca0a3a17fdf6fec135c73d72d2c5d00b44d6284f
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-19 17:27:30 +0000

    Add js checks on quantity via product_detail template

--- bag/views.py
+++ bag/views.py
@@ -15,18 +15,37 @@ def add_to_bag(request, item_id):
     size = None
 
     if 'product_size' in request.POST:
-        size = request.POST['product_size']
-        print(size)
-    bag = request.session.get('bag', {})
+        """product_size passed via the related input and name attribute"""
+        size = request.POST['product_size']  # Then, assign it
+    bag = request.session.get('bag', {})  # Now, update the bag session data
 
     if size:
+        """if size has been honoured through the logic above"""
         if item_id in list(bag.keys()):
+            """
+                if the item_id passed through the request is already among the
+                bag session data
+            """
             if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
+                """
+                    if such item_id found in the bag session data already has a
+                    dictionary key named 'size'
+                """
+                bag[item_id]['items_by_size'][size] += quantity # Then, to such
+                #  specific key, sum the quantity being added from the request
+                # to the one available via the specified key/size
             else:
-                bag[item_id]['items_by_size'][size] = quantity
+                """
+                    If there is no key/size in the dictionary of such item_id
+                """
+                bag[item_id]['items_by_size'][size] = quantity  # Then,
+                #  set the new quantity for such key/size
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            """
+                If the item id was not previously added to the bag session data
+            """
+            bag[item_id] = {'items_by_size': {size: quantity}}  # Then, create
+            # it
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
