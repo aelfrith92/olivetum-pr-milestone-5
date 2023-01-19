85520c5009da0bf875f1126dbac105f977222244
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-19 10:43:36 +0000

    Add features about sizes for olive oil products in litres and render it front-end

--- products/models.py
+++ products/models.py
@@ -23,10 +23,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    # The following field can be enabled after the fixtures have been applied/
-    # imported
-    # has_child_variants = models.BooleanField(default=False, null=True, blank=
-    # True)
+    has_child_variants = models.BooleanField(default=False, null=True,
+                                             blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
