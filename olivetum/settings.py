b069db51ee67a7a23ca507ce34c1e3faffef9626
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-21 14:37:25 +0000

    Style toasts, fix the related js, replace BS5 version with the latest

--- olivetum/settings.py
+++ olivetum/settings.py
@@ -112,21 +112,21 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'olivetum.wsgi.application'
 
-# Bootstrap 5 settings - last available version 5.2.3
+# Bootstrap 5 settings - last available version 5.3.0-alpha1
 BOOTSTRAP5 = {
     "css_url": {
-        "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/"
+        "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/"
                 "bootstrap.min.css",
-        "integrity": "sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH6"
-                     "1DGLwZJEdK2Kadq2F9CUG65",
+        "integrity": "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/"
+                     "Jr59b6EGGoI1aFkw7cmDA6j6gD",
         "crossorigin": "anonymous",
     },
 
     "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/"
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/"
                "bootstrap.bundle.min.js",
-        "integrity": "sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXk"
-                     "k+Q2h455rYXK/7HAuoJl+0I4",
+        "integrity": "sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+"
+                     "EYdgRZgiwxhTBTkF7CXvN",
         "crossorigin": "anonymous",
     },
 
