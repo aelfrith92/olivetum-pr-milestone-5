4084402ce8d88c75fccd5889bf58bc0e1aab2a17
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-21 01:02:02 +0000

    Found a temp solution for toasts bs5.2

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -46,11 +49,13 @@ def add_to_bag(request, item_id):
             """
             bag[item_id] = {'items_by_size': {size: quantity}}  # Then, create
             # it
+            messages.success(request, f'Added {product.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
