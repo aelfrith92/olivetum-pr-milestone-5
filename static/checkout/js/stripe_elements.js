9d605f7cb757c5939fc0c587a66d4a75d1e942a5
Alfredo Morieri <fred.morieri@gmail.com>
2023-01-22 17:11:09 +0000

    Set Stripe env vars via settings, handle event errors via js, provide further info to the checkout template via checkout view

--- static/checkout/js/stripe_elements.js
+++ static/checkout/js/stripe_elements.js
@@ -12,7 +12,7 @@ var elements = stripe.elements();
 var style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontFamily: "'Helvetica Neue', Helvetica, sans-serif",
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
@@ -26,3 +26,18 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+card.addEventListener('change', function (event){
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
